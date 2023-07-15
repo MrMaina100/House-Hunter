2df41abc908dc8193c4330426e9987c49b4e0af9
Derrick Maina <mburumaina648@gmail.com>
2023-07-15 11:44:13 +0300

    added both sign in and sign up

--- src/Pages/SignIn.jsx
+++ src/Pages/SignIn.jsx
@@ -1,6 +1,59 @@
+import { Link, useNavigate } from "react-router-dom"
+import { useState } from "react"
+
 function SignIn() {
+  const [showPassword, setShowpassword]= useState(false)
+  const [formData, setFormData] = useState({
+    email:'',
+    password:''
+  })
+
+  const {email, password} = formData
+
+  const onChange = (e)=>{
+    setFormData((prevState)=>({
+      ...prevState,
+      [e.target.id]: e.target.value
+
+    }))
+
+  }
   return (
-    <div>SignIn</div>
+    <>
+    <div className="mx-auto max-w-[80%] flex-col space-y-2 justify-center items-center">
+      <h1>Welcome back</h1>
+
+      <form>
+
+        <input type="email"
+         value={email} 
+         placeholder="email"
+         onChange={onChange}
+         id="email"
+
+          />
+
+        <input 
+         type={showPassword ? 'text' : 'password'}
+         placeholder="password"
+         onChange={onChange}
+         value={password}
+         id="password"         
+         />
+
+         <Link to='/forgotpassword'>
+          forgot password
+         </Link>
+
+      </form>
+
+      <Link to='/signup'>
+        Sign up instead
+      </Link>
+
+    </div>
+    
+    </>
   )
 }
 export default SignIn
--- src/Pages/SignUp.jsx
+++ src/Pages/SignUp.jsx
@@ -1,6 +1,68 @@
+import { Link, useNavigate } from "react-router-dom"
+import { useState } from "react"
+
 function SignUp() {
+  const [showPassword, setShowpassword]= useState(false)
+  const [formData, setFormData] = useState({
+    name:'',
+    email:'',
+    password:''
+  })
+
+  const {name,email, password} = formData
+
+  const onChange = (e)=>{
+    setFormData((prevState)=>({
+      ...prevState,
+      [e.target.id]: e.target.value
+
+    }))
+
+  }
   return (
-    <div>SignUp</div>
+    <>
+    <div className="mx-auto max-w-[80%] flex-col space-y-2 justify-center items-center">
+      <h1>Welcome back</h1>
+
+      <form>
+
+         <input type="text"
+         value={name} 
+         placeholder="Enter name"
+         onChange={onChange}
+         id="name"
+
+          />
+
+        <input type="email"
+         value={email} 
+         placeholder="email"
+         onChange={onChange}
+         id="email"
+
+          />
+
+        <input 
+         type={showPassword ? 'text' : 'password'}
+         placeholder="password"
+         onChange={onChange}
+         value={password}
+         id="password"         
+         />
+
+         <Link to='/forgotpassword'>
+          forgot password
+         </Link>
+
+      </form>
+
+      <Link to='/signin'>
+        Sign in instead
+      </Link>
+
+    </div>
+    
+    </>
   )
 }
 export default SignUp
