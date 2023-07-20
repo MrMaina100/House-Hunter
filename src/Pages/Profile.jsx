4fabb0b4fc9c45079ad4fb786e54b781ccf1ce45
Derrick Maina <mburumaina648@gmail.com>
2023-07-20 21:23:11 +0300

    user is now able to login and edit details

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -1,18 +1,92 @@
-import { getAuth } from "firebase/auth"
+import { getAuth,updateProfile } from "firebase/auth"
 import { useEffect, useState } from "react"
+import { useNavigate , Link} from "react-router-dom"
+import {updateDoc, doc} from "firebase/firestore"
+import {db} from '../Firebase.config'
+import { toast } from "sonner"
+
 
 
 function Profile() {
-  const [user, setUser] = useState(null) 
   const auth = getAuth()
-  useEffect(()=>{
-    setUser(auth.currentUser);
+  const [FormData, setFormData] = useState({
+    name: auth.currentUser.displayName || '',
+    email: auth.currentUser.email,
+  }) 
 
-  },[])
+  const {name, email} = FormData
+
+  const navigate = useNavigate()
+
+  const handleLogout = ()=>{
+    auth.signOut()
+    navigate('/')
+  }
+
+  const [changeDetails,setChangeDetails] = useState(false)
+
+  const onSubmit = async ()=>{
+    try {
+      if(auth.currentUser.displayName !== name){
+        await updateProfile(auth.currentUser,{
+          displayName:name
+        })
+      }
+      //update to firestore
+      const userRef = doc(db, 'users', auth.currentUser.uid)
+      await updateDoc(userRef,{
+        name
+      })
+      
+    } catch (error) {
+      toast.error('Could not update username')
+      
+    }
+  }
+
+
+  const handleChange = (e)=>{
+    setFormData((prev)=>({
+      ...prev,
+      [e.target.id]:e.target.value
+    }))
+
+  }
+   
+
+
+  
+
   return (
-    <div>
-      {user ? <h1>{user.displayName}</h1>: 'not logged in'}
-    </div>
+    <>
+     <h1> my profile</h1>
+     <div>
+      <button onClick={handleLogout}>Log out</button>
+     </div>
+
+     <div>
+       <p>personal Details</p>
+        <p onClick={()=>{
+          changeDetails && onSubmit()
+          setChangeDetails((prevState)=>!prevState)
+        }}>
+          {changeDetails ? 'done': 'change'}
+          </p>
+
+     </div>
+
+     <div>
+      <form className="h-8">
+        <input type="text" id="name" value={name} onChange={handleChange}  disabled={!changeDetails}/>
+        <input type="text" id="name" value={email} onChange={handleChange} disabled={!changeDetails}  />
+
+      </form>
+     </div>
+    
+
+
+     
+    </>
   )
 }
 export default Profile
