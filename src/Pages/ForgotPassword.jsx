70b1fe27c6fd8e0e19d105dce5d671eae73d13a7
Derrick Maina <mburumaina648@gmail.com>
2023-08-01 12:49:57 +0300

    added the forgot password functionality

--- src/Pages/ForgotPassword.jsx
+++ src/Pages/ForgotPassword.jsx
@@ -1,6 +1,40 @@
+import { useState } from "react"
+import { getAuth, sendPasswordResetEmail } from "firebase/auth"
+import { Link } from "react-router-dom"
+import { toast } from "sonner"
+
 function ForgotPassword() {
+  const [formData, setFormData] = useState()
+
+  const handleResetSumbit = async (e)=>{
+    e.preventDefault()
+    try {
+      const auth = getAuth()
+      await sendPasswordResetEmail(auth, formData)
+      toast.success('check email for password reset')            
+    } catch (error) {
+      toast.error('something went wrong')
+      
+    }
+
+  }
   return (
-    <div>ForgotPassword</div>
+    <>
+    <h1>reset password</h1>
+    <form onSubmit={handleResetSumbit}>
+      <input
+       type="text"
+       placeholder="enter your email for reset"
+       value={formData}
+       onChange={(e)=>setFormData(e.target.value)}
+       id="formData"
+        />
+
+        <button>send it</button>
+    </form>
+    
+    </>
+    
   )
 }
 export default ForgotPassword
